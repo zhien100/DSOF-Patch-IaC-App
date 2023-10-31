de1271dd34cd76121d28baf0b37d0173a55b83f6
zhien100 <zhien100@gmail.com>
2023-10-31 17:01:48 +0800

    fixed vulnarabilities

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -1,5 +1,6 @@
 resource "aws_cloudtrail" "insecure-logging" {
   name           = "cloudtrail-logging"
   s3_bucket_name = "my-cloudtrail-bucket"
-  enable_logging = false
+  enable_logging = true
+  enable_log_file_validation: true
 }
